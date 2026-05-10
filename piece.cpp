7803857cde9c02333b963602821b1227fa8a8263
Abdul Moeed <abdulmoeed1607@gmail.com>
2026-05-11 03:39:54 +0500

    piece.cpp

---
+++ piece.cpp
@@ -0,0 +1,33 @@
+#include "piece.h"
+#include "board.h"
+piece::piece(string color, int rows, int cols)
+{
+    this->color = color;
+    this->rows = rows;
+    this->cols = cols;
+}
+string piece::getcolor()
+{
+    return color;
+}
+int piece::getrow()
+{
+    return rows;
+}
+int piece::getcol()
+{
+    return cols;
+}
+void piece::setposition(int newrow, int newcol)
+{
+    rows = newrow;
+    cols = newcol;
+}
+string piece::getsymbol()
+{
+    return "piece";
+}
+bool piece::isvalid(int newrow, int newcol, board* board)
+{
+    return true;
+}
